469e3fb8619f4115011141f7841fb997b64ea51f
eX1337PL <checkxpl@gmail.com>
2014-02-14 12:43:52 +0100

    Create AutoMessageScheduler.java

---
+++ src/pl/AutoMessage/AutoMessageScheduler.java
@@ -0,0 +1,12 @@
+package pl.AutoMessage;
+
+import org.bukkit.event.Listener;
+
+public class AutoMessageScheduler implements Listener {
+	
+	Main plugin;
+	
+	public AutoMessageScheduler(Main plugin) {
+		this.plugin = plugin;
+	}
+}
