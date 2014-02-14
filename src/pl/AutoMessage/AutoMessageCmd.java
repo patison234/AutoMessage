044b248135625c2660dafda80c68d3aecf705c95
eX1337PL <checkxpl@gmail.com>
2014-02-14 13:02:55 +0100

    Update AutoMessageCmd.java

--- src/pl/AutoMessage/AutoMessageCmd.java
+++ src/pl/AutoMessage/AutoMessageCmd.java
@@ -14,6 +14,11 @@ public class AutoMessageCmd extends JavaPlugin implements CommandExecutor {
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("automessage")) {
+			if (args.length == 0) {
+				Utils.sendMsg("&6==========================================\n &6> &7Autorzy: &6PatisonPlayGames &7& &6DragoPL\n &6> &7Wersja: &6v1.0\n&6==========================================\n &6> &7/automessage -r &6&l| &7przeladowuje konfiguracje\n&6==========================================", sender);
+			}
+		}
 		return false;
 	}
 }
